fff9a0ab1ca22cbe87b538b2498af06cf060c68e
David Bergeron <dbergeron2@nait.ca>
2019-12-04 09:46:45 -0700

    adding production build and stuff

--- ass5/webpack.config.js
+++ ass5/webpack.config.js
@@ -1,35 +1,48 @@
-import webpack from 'webpack';
-import path from 'path';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack from "webpack";
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import {CleanWebpackPlugin} from 'clean-webpack-plugin'
 
 export default {
-   entry: './js/src/main.js',
-   output : {
-       path: path.resolve(__dirname, 'build'),
-       filename: 'main.js'
-   },
-   module: {
-       rules: [
-           {test: /\.(m?js|jsx)$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader'
-            }
-        
+  entry: "./js/src/main.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "[name].[hash].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(m?js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
         }
-       ]
-   },
-   plugins: [
-       new HtmlWebpackPlugin({
-           template: './index.html',
-           inject: true,
-           favicon: './favicon.ico',
-           filename: './index.html'
-       }),
-       new webpack.HotModuleReplacementPlugin()
-   ],
-   devServer : {
-       contentBase: './build/',
-       hot: true
-   }
-}
+      },
+      { test: /.css$/, exclude: /node_modules/,  use: ["style-loader", {
+          loader: MiniCssExtractPlugin.loader,
+          options: {
+              hmr: true
+          }
+      } ,"css-loader"] }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      inject: false,
+      favicon: "./favicon.ico",
+      filename: "./index.html",
+      hash: true
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new MiniCssExtractPlugin({
+        filename: 'main.[contenthash].css'
+    }),
+    new CleanWebpackPlugin()
+  ],
+  devServer: {
+    contentBase: "./build/",
+    hot: true
+  }
+};
